83bf379556171446c39fb15f8f560fde1f460deb
Emith Randiv <randivemith.2003@gmail.com>
2025-05-09 15:51:24 +0530

    Add merging changes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const sendEmailRoute = require('./routes/sendServiceDueEmail');
 const dotenv = require("dotenv");
 
 require("dotenv").config();
