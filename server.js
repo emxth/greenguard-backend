fa72832bef411658c13138409773a28c1ab147bb
Emith Randiv <randivemith.2003@gmail.com>
2025-05-09 14:40:00 +0530

    Merging

--- server.js
+++ server.js
@@ -44,17 +44,13 @@ const truckMaintenanceRouter = require("./routes/truckMaintenance");
 app.use("/Maintenance", truckMaintenanceRouter);
 
 const truckFuelCostRouter = require("./routes/truckFuelCost");
+//Maintenance DB
 //Backend URL
 //http://Localhost:8080/FuelCost
 app.use("/FuelCost", truckFuelCostRouter);
 
 const Stripe = require("stripe");
 const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
-
-const truckRequestRouter = require("./routes/truckrequests");
-//Backend URL
-//http://Localhost:8080/truckRequest
-app.use("/truckRequest", truckRequestRouter);
 
 const paymentRouter = require("./routes/payment");
 app.use("/payment", paymentRouter);
